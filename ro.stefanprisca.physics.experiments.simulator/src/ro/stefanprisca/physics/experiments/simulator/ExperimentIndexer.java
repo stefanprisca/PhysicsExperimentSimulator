e5d029941bd69acd90918b1e055f8209665646ec
Stefan Prisca <stefan.prisca@gmail.com>
2013-11-19 11:25:03 +0200

    Included class diagram
    Signed-off-by: Stefan Prisca <stefan.prisca@gmail.com>

---
+++ ro.stefanprisca.physics.experiments.simulator/src/ro/stefanprisca/physics/experiments/simulator/ExperimentIndexer.java
@@ -0,0 +1,13 @@
+package ro.stefanprisca.physics.experiments.simulator;
+
+import java.util.List;
+
+import ro.stefanprisca.physics.experiments.simulator.core.Experiment;
+
+public class ExperimentIndexer {
+	private List<Experiment> experiments;
+	
+	public List<Experiment> indexExperiments(){
+		return null;
+	}
+}
